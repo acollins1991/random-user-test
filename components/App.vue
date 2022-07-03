93145a5aac2dd559e57fcb4396ef906e53ee5325
Anthony Collins <a.r.c_@hotmail.co.uk>
2022-07-04 00:33:13 +0100

    started frontend

--- components/App.vue
+++ components/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>testing</div>
+  <div>
+    <people-slider v-bind:people="people" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapActions, mapState } from 'vuex'
+import PeopleSlider from './PeopleSlider.vue'
 export default Vue.extend({
-  name: 'App',
+  components: { PeopleSlider },
   computed: {
     ...mapState({
       people: 'people',
---
+++ components/PeopleSlider.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="person in people" v-bind:key="person.email">test</li>
+    </ul>
+    <button ref="prev">Previous</button>
+    <button ref="next">Next</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapActions } from 'vuex'
+export default Vue.extend({
+  name: 'PeopleSlider',
+  props: {
+    people: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    pagination() {
+      return {
+        current: 1,
+        totalPeopleLoaded: 3,
+        totalPages: 1,
+      }
+    },
+  },
+  mounted() {
+    const nextButton: any = this.$refs.next
+    nextButton.addEventListener('click', () => {
+      this.addPeople(3)
+    })
+  },
+  methods: {
+    ...mapActions({
+      addPeople: 'people/addPeople',
+    }),
+  },
+})
+</script>
